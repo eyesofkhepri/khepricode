442ec79f638f4af58961ba2fe9c70b89f91ae09d
Eyes of Khepri <eyesofkhepri@gmail.com>
2017-11-28 20:55:01 +0900

    1. spring boot project 구성중. 2. jooq프로젝트 생성 및 간략히 테스트

---
+++ springbootjooq/src/main/java/com/eyesofkhepri/springbootjooq/SpringbootjooqApplication.java
@@ -0,0 +1,12 @@
+package com.eyesofkhepri.springbootjooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjooqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjooqApplication.class, args);
+	}
+}
